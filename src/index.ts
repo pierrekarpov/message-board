eca7257afc6a86ac7948c68debc7b3aa1d8af338
Pierre Karpov <karpovpierre@gmail.com>
2022-04-29 16:09:52 +0800

    change DB posrt env var

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import models from './db/models';
 const { sequelize } = models;
 const PORT = process.env.NODE_LOCAL_PORT || 8000;
 const DB_HOST = process.env.DB_HOST || '127.0.0.1';
-const DB_PORT = process.env.MYSQLDB_LOCAL_PORT || 3307;
+const DB_PORT = process.env.DB_PORT || 3307;
 
 
 app.on('ready', () => {
