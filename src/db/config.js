2c1d71b001523015f54820d17bccc82d69181a69
Pierre Karpov <karpovpierre@gmail.com>
2022-04-30 19:17:19 +0800

    db password env var

--- src/db/config.js
+++ src/db/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     development: {
         username: process.env.DB_USER,
-        password: process.env.DB_ROOT_PASSWORD,
+        password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
@@ -20,7 +20,7 @@ module.exports = {
     },
     test: {
         username: process.env.DB_USER,
-        password: process.env.DB_ROOT_PASSWORD,
+        password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
@@ -39,7 +39,7 @@ module.exports = {
     },
     production: {
         username: process.env.DB_USER,
-        password: process.env.DB_ROOT_PASSWORD,
+        password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
